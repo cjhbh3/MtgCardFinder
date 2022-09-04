cd52c0897ef994505b9ed4a7c28d08b6f7f6260d
CJ Hess <cjh147@lindenwood.edu>
2022-09-04 04:44:57 -0500

    Finished base program
    Base program is in C++, transferring to Python to make it work Raspberry Pi

---
+++ MtgCardFinder.py
@@ -0,0 +1,7 @@
+from py_compile import main
+
+# Have to write in Python for future implmentation (from C++ so fun)
+# Fucking kill me
+
+if __name__ == "__main__":
+    print("Hello World")
--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@
 using namespace std;
 
 bool searchList(vector<string> cardList, string cardName) {
+    if (!cardList.empty()) {
+        for (const auto &card: cardList){
+            if (card == cardName) {
+                return true;
+            }
+        }
+    }
+    else {
+        cout << "List is empty" << endl;
+    }
+    
     return false;
 }
 
@@ -36,7 +47,7 @@ vector<string> loadCards() {
         cout << "File failed to open" << endl;
     }
     else {
-        cout << "Pog" << endl;
+        cout << "File opened successfully" << endl;
     }
 
     while (getline(in, cardName)) {
@@ -51,19 +62,43 @@ vector<string> loadCards() {
 int main() {
     // argv[0] = executable
     string inputFileName;
+    string menuOption = "";
     vector<string> cards;
 
     cout << "Hello World" << endl;
 
     // Prompt for input file
     cout << "What is the file name?" << endl;
-    
     cards = loadCards();
 
-    cout << cards.size() << endl;
+    while (menuOption != "3") {
+        cout << "What do you want to do" << endl;
+        cout << "1. Print Deck List" << endl;
+        cout << "2. Find Card in Deck List" << endl;
+        cout << "3. Quit" << endl;
 
-    // print list of cards
-    printList(cards);
+        cin >> menuOption;
+
+        if (menuOption == "1") {
+            printList(cards);
+        }
+        else if (menuOption == "2") {
+            string cardName = "";
+            cout << "What card are you looking for?" << endl;
+            cin >> cardName;
+            if (searchList(cards, cardName)) {
+                cout << "Card is in the list" << endl;
+            }
+            else {
+                cout << "Card is not in the list" << endl;
+            }
+        }
+        else if (menuOption != "3") {
+            cout << "Please try again" << endl;
+        }
+    }
+
+
 
     return 0;
 }
